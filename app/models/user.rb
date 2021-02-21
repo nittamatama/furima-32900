cdfdf0ff6cba8a532c6380ae9594ccbbaa2cb3d0
nittamatama <blackmnshin@gmail.com>
2021-02-21 21:23:23 +0900

    新規登録実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, :first_name, :last_name, :first_kana, :last_kana, :birthday, presence: true
+         validates :email, uniqueness: true
+         validates :password, :password_confirmation, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
