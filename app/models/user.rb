8d23cbdefbac0654ab384a6c453389e99cf7bbf5
nittamatama <blackmnshin@gmail.com>
2021-02-27 18:07:59 +0900

    バリデーションをリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,20 @@ class User < ApplicationRecord
 
          
          with_options presence: true do
-         validates :nickname
-         validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "には全角文字を使用してください"}
-         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "には全角文字を使用してください"}
-         validates :last_kana, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "には全角文字を使用してください"}
-         validates :first_kana, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "には全角文字を使用してください"}
-         validates :last_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください" }
-         validates :first_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください" }
+           with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "には全角文字を使用してください"} do
+             validates :last_name 
+             validates :first_name
+           end
+           with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "には全角文字を使用してください"} do
+             validates :last_kana
+             validates :first_kana
+           end
+           with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "には全角カタカナを使用してください" } do
+             validates :last_kana
+             validates :first_kana
+           end
          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数字混合で設定してください" }
          validates :birthday
+         validates :nickname
          end
 end
