69e58ce2e04cec50c5cd30f7892bc6610853be94
nittamatama <blackmnshin@gmail.com>
2021-03-05 20:12:23 +0900

    商品出品機能実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,30 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :delivery_fee, :prefecture, :delivery_period
+
+  with_options presence: true do
+    validates :product_name
+    validates :description
+    validates :selling_price
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :delivery_period_id
+    validates :prefecture_id
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :delivery_period_id
+  end
+  
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :selling_price, format: { with: /\A[0-9]+\z/, message: "は半角数字で入力してください" }
+  validates :selling_price, inclusion: {in:300..9999999}
 end
