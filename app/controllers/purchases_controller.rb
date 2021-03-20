4c1b097825c4700d7e8fe20bda66f4fdb7a82e28
nittamatama <blackmnshin@gmail.com>
2021-03-20 15:41:08 +0900

    商品購入機能index終了

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,10 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_index, only: [:index, :create]
+  
   def index
     @address_purchase = AddressPurchase.new
+    @product = Product.find(params[:product_id])
   end
   
   def create
@@ -16,5 +20,12 @@ class PurchasesController < ApplicationController
     private
     def address_params
       params.require(:address_purchase).permit(:postal_code, :city, :address, :building_name, :prefecture_id, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id])
+    end
+
+    def move_to_index
+      @user = Product.find(params[:product_id])
+      if @user.user_id == current_user.id
+         redirect_to root_path
+      end
     end
 end
--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :address do
-    
+    postal_code   {181-0002}
+    city          {'三鷹市'}
+    address       {'牟礼'}
+    building_name {'アーチェリーハウス'}
+    prefecture_id { 1 }
+    phone_number  { 08001611279 }
   end
 end
