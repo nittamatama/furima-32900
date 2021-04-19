5b577780b89d7b0642a1bf71dcda286a1e189132
nittamatama <blackmnshin@gmail.com>
2021-04-20 00:32:09 +0900

    実装１

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -8,14 +8,12 @@ document.addEventListener('DOMContentLoaded', function(){
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
 
+      const inputHTML = document.createElement('input')
+
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
     };
       document.getElementById('item-image').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
       
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
